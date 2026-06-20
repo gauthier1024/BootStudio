c085b59d7eafd1a1d9bf6a937efc87451879dc7d
gauthier1024 <243645236+gauthier1024@users.noreply.github.com>
2026-06-20 21:15:51 +0000

    Setup directory

---
+++ app/src/main/java/com/bootstudio/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.bootstudio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.bootstudio.ui.theme.BootStudioTheme
+import com.bootstudio.ui.screens.HomeScreen
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BootStudioTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
